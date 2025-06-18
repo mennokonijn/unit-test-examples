ff7c2c58cbda11bb9d471392ec4cfa757d34d558
Menno Konijn <menno.konijn@usmedia.nl>
2025-06-18 13:25:14 +0200

    test dup

--- src/js/FortuneTellerDup.js
+++ src/js/FortuneTellerDup.js
@@ -51,6 +51,48 @@ class FortuneTellerDup {
             return '';
         }
     }
+
+    /**
+     * Return the answer to life, universe and everything; in case you spend enough money, that is.
+     * @param {number} [money=0] - Money spent for getting an answer.
+     * @return {string} An answer.
+     */
+    answer4( money = 0 ) {
+
+        if (money >= 5) {
+            return this.oracle.answer();
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * Return the answer to life, universe and everything; in case you spend enough money, that is.
+     * @param {number} [money=0] - Money spent for getting an answer.
+     * @return {string} An answer.
+     */
+    answer5( money = 0 ) {
+
+        if (money >= 5) {
+            return this.oracle.answer();
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * Return the answer to life, universe and everything; in case you spend enough money, that is.
+     * @param {number} [money=0] - Money spent for getting an answer.
+     * @return {string} An answer.
+     */
+    answer6( money = 0 ) {
+
+        if (money >= 5) {
+            return this.oracle.answer();
+        } else {
+            return '';
+        }
+    }
 }
 
 export default FortuneTellerDup;
