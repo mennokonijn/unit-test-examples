b6c10ad8d9c8c85791d30ee44cb30507f657677c
Menno Konijn <menno.konijn@usmedia.nl>
2025-07-11 10:45:40 +0200

    test

--- src/js/Answer.js
+++ src/js/Answer.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Answer = ( props ) => {
 	const { id, text } = props;
 
+	// test
+
 	return (
 		<li className="oracle__answer">
 			Answer { id }: <em>{ text }</em>
