bc20a36d07ee75f6ec8fde70109ef42e16cab54f
Menno Konijn <menno.konijn@usmedia.nl>
2025-07-15 13:33:47 +0200

    test

--- src/js/functions.js
+++ src/js/functions.js
@@ -2,6 +2,9 @@
  * Return the answer to life, universe and everything.
  * @return {string} The answer to life, universe and everything.
  */
+// test-secret.js
+const API_KEY = "AKIAFAKEKEY1234567890";
+
 export function getAnswer() {
 	return '42';
 }
